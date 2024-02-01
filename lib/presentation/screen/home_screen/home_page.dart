9a692bc93e9f833dce4df4309bc48b7aca7cd66a
Sugith <sugithkodappurath@gmail.com>
2024-02-02 01:25:18 +0530

    splash screen done

---
+++ lib/presentation/screen/home_screen/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: Text('homeScreen'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screen/splash_screen/splash_page.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:crypto_tracker/presentation/screen/home_screen/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+ 
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 5), () {Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const HomePage())); });
+    return Scaffold(
+      
+      body: Center(
+
+        child:
+         Padding(
+           padding: const EdgeInsets.all(30.0),
+           child: Lottie.asset('asset/json/cryptocurrency_lottie_animation.json'),
+         ),
+      ),
+    );
+  }
+}
