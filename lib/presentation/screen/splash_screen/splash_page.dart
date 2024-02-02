ce0539cedf100bdfeb9a28dc59fcabde252dd655
Sugith <sugithkodappurath@gmail.com>
2024-02-02 23:43:14 +0530

    splash screen logo change

--- lib/presentation/screen/splash_screen/splash_page.dart
+++ lib/presentation/screen/splash_screen/splash_page.dart
@@ -9,15 +9,15 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 5), () {Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const HomePage())); });
+    // Timer(const Duration(seconds: 5), () {Navigator.of(context).push(MaterialPageRoute(builder: (_)=> const HomePage())); });
     return Scaffold(
       
       body: Center(
 
         child:
          Padding(
-           padding: const EdgeInsets.all(30.0),
-           child: Lottie.asset('asset/json/cryptocurrency_lottie_animation.json'),
+           padding: const EdgeInsets.all(50.0),
+           child: Lottie.asset('asset/json/crypto_splash_scrn.json'),
          ),
       ),
     );
